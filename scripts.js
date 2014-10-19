0e2b7a25463e30662ea8d5e54dba20073e50bfff
amyruth <anrutherford@gmail.com>
2014-10-19 13:20:03 -0400

    adds add additional address functionality

--- scripts.js
+++ scripts.js
@@ -1,14 +1,47 @@
 $(document).ready(function() {
- 	$('#new-contact').submit(function(event){
+	$('#add-address').click(function(){
+		$('#new-addresses').append('<div class="new-address">' +
+					                  '<div class="form-group">' +
+					                    '<label for="new-street">Street</label>' +
+					                    '<input type="text" class="form-control new-street">' +
+					                '</div>'+
+                
+					                '<div class="form-group">'+
+					                 ' <label for="new-city">City</label>'+
+					                 ' <input type="text" class="form-control new-street">'+
+					               ' </div>'+
+
+					                '<div class="form-group">'+
+					                  '<label for="new-state">State</label>'+
+					                 ' <input type="text" class="form-control new-state">'+
+					                '</div>'+
+					            '</div>'
+					          	);
+	});
+	
+	$('#new-contact').submit(function(event){
  		event.preventDefault();
 
  		var inputtedFirstName = $('#new-first-name').val();
  		var inputtedLastName = $('#new-last-name').val();
  		var inputtedAddress = $('#new-address').val();
+ 		
  		var newContact = {firstName: inputtedFirstName,
  						  lastName: inputtedLastName,
- 						  address: inputtedAddress
+ 						  addresses: []
  						  };
+
+ 		$('.new-address').each(function(){
+ 			var inputtedStreet = $(this).find('.new-street').val();
+ 			var inputtedCity = $(this).find('.new-city').val();
+ 			var inputtedState = $(this).find('.new-state').val();
+
+ 			var newAddress = {street: inputtedStreet,
+ 							  city: inputtedCity,
+ 							  state: inputtedState
+ 							  };
+ 			newContact.addresses.push(newAddress);
+ 		})
 
  		$('#contact').append('<li><span class="contact">' + newContact.firstName + " " + newContact.lastName +'</span></li>');
 
